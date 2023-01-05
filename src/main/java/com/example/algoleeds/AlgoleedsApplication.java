b9cfb10b130da80f30535baee5f649c74a86b576
minjko <komj36@gmail.com>
2023-01-05 11:51:16 +0900

    create project

---
+++ src/main/java/com/example/algoleeds/AlgoleedsApplication.java
@@ -0,0 +1,13 @@
+package com.example.algoleeds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoleedsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgoleedsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/algoleeds/AlgoleedsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.algoleeds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoleedsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
